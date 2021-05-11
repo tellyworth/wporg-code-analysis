79c5208597e48b3a15d4ad34da8590b8fb383161
tellyworth <alex@automattic.com>
2021-05-11 16:16:48 +1000

    Create check-plugin-tag-diff.php

---
+++ bin/check-plugin-tag-diff.php
@@ -0,0 +1,273 @@
+#!/usr/bin/php
+<?php
+/**
+ * A quick and dirty script for testing the PHPCS class against any plugin in the directory.
+ * This script does not use or require WordPress.
+ *
+ * Usage:
+ * php check-plugin-by-slug.php --slug akismet
+ * Or:
+ * php check-plugin-by-slug.php
+ */
+
+use WordPressDotOrg\Code_Analysis\PHPCS;
+
+// This script should only be called in a CLI environment.
+if ( 'cli' != php_sapi_name() ) {
+	die();
+}
+
+$opts = getopt( '', array( 'slug:', 'tag:', 'report:', 'page:', 'number:', 'errors', 'browse:' ) );
+if ( empty( $opts['report'] ) ) {
+	$opts['report'] = 'summary';
+}
+if ( intval( $opts['page'] ?? 0 ) < 1 ) {
+	$opts['page'] = 1;
+}
+if ( intval( $opts['number'] ?? 0 ) < 1 ) {
+	$opts['number'] = 25;
+}
+if ( empty( $opts['tag'] ) || empty( $opts['slug'] ) ) {
+	$opts['tag'] = null;
+}
+
+// Fetch the slugs of the top plugins in the directory
+function get_top_slugs( $plugins_to_retrieve, $starting_page = 1, $browse = 'popular' ) {
+	$payload = array(
+		'action' => 'query_plugins',
+		'request' => serialize( (object) array( 'browse' => $browse, 'per_page' => $plugins_to_retrieve, 'page' => $starting_page, 'fields' => [ 'active_installs' => true ] ) ) );
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,"https://api.wordpress.org/plugins/info/1.0/");
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload );
+
+	$response = curl_exec( $ch );
+
+	$data = unserialize( $response );
+
+	curl_close( $ch );
+
+	var_dump( $data ); die;
+
+	$out = [];
+
+	foreach ( $data->plugins as $plugin ) {
+		$out[ $plugin->slug ] = [
+			'slug' => $plugin->slug,
+			'installs' => $plugin->active_installs,
+			'updated' => $plugin->last_updated,
+		];
+	}
+
+	return $out;
+}
+
+// Return two zip file URLs: $version_to_check, and the next available version after that one.
+function get_zip_versions( $plugin_slug, $version_to_check ) {
+	$payload = array(
+		'action' => 'plugin_information',
+		'request' => serialize( (object) array( 'slugs' => $plugin_slug, 'fields' => [ 'active_installs' => true ] ) ) );
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,"https://api.wordpress.org/plugins/info/1.0/");
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $payload );
+
+	$response = curl_exec( $ch );
+
+	$data = (array)unserialize( $response );
+
+	curl_close( $ch );
+
+	if ( isset( $data[$plugin_slug]['versions'] ) && count( $data[$plugin_slug]['versions'] ) > 1 ) {
+		$version_keys = array_keys( $data[$plugin_slug]['versions'] );
+
+		if ( $i = array_search( $version_to_check, $version_keys ) ) {
+			if ( isset( $version_keys[ $i + 1 ] ) ) {
+				return [
+					$version_keys[ $i ] => $data[$plugin_slug]['versions'][ $version_keys[ $i ] ],
+					$version_keys[ $i+1 ] => $data[$plugin_slug]['versions'][ $version_keys[ $i+1 ] ],
+
+				];
+			}
+		}
+	}
+
+	return false;
+
+}
+
+function get_svn_tag_versions( $plugin_slug, $version_to_check ) {
+	$cmd = "svn ls " . escapeshellarg( "https://plugins.svn.wordpress.org/" . urlencode($plugin_slug) . '/tags/' );
+	$ls = shell_exec( $cmd );
+	$tags = array_map( function($str) { return trim( trim( $str ), '/' ); }, explode( "\n", $ls ) );
+	$tags = array_filter( $tags );
+	usort( $tags, 'version_compare' );
+
+	if ( $index = array_search( $version_to_check, $tags ) ) {
+		if ( isset( $tags[ $index + 1 ] ) ) {
+			return [
+				$tags[ $index ],
+				$tags[ $index + 1 ],
+			];
+		}
+	}
+
+	return false;
+}
+
+function get_dir_for_tag( $tag ) {
+	if ( !empty( $tag ) ) {
+		$dir = '/tags/' . basename( $tag );
+	} else {
+		$dir = '/trunk';
+	}
+
+	return $dir;
+}
+
+// Export a plugin to ./plugins/SLUG and return the full path to that directory
+function export_plugin( $slug, $tag = null ) {
+
+	$tmpnam = tempnam( '/tmp', 'plugin-' . $slug );
+
+	$dir = get_dir_for_tag( $tag );
+
+	if ( $tmpnam ) {
+		$tmpnam = realpath( $tmpnam );
+		unlink( $tmpnam );
+		mkdir( $tmpnam ) || die( "Failed creating temp directory $tmpnam" );
+		$cmd = "svn export --force https://plugins.svn.wordpress.org/" . $slug . $dir . ' ' . $tmpnam;
+		shell_exec( $cmd );
+
+		return $tmpnam;
+	}
+}
+
+// Export a plugin ZIP to ./themes/SLUG and return the full path to that directory
+function export_plugin_zip( $slug, $url ) {
+
+	$zipfile = tempnam( '/tmp', $slug . '.zip' );
+	copy( $url, $zipfile );
+
+	$tmpnam = tempnam( '/tmp', 'plugin-' . $slug );
+
+	if ( $tmpnam ) {
+		$tmpnam = realpath( $tmpnam );
+		unlink( $tmpnam );
+		mkdir( $tmpnam ) || die( "Failed creating temp directory $tmpnam" );
+
+		$zip = new ZipArchive();
+		if ( $zip->open( $zipfile ) ) {
+			$zip->extractTo( $tmpnam . '/' );
+			$zip->close();
+			unlink( $zipfile );
+			return $tmpnam;
+		}
+	}
+
+	return false;
+}
+
+
+// Fake WP_Error class so the PHPCS class works
+class WP_Error {
+	var $code;
+	var $message;
+	var $data;
+
+	function __construct( $code = '', $message = '', $data = '' ) {
+		$this->code = $code;
+		$this->message = $message;
+		$this->data = $data;
+	}
+
+	public function __toString() {
+		return var_export( $this, true );
+	}
+}
+
+// Again so PHPCS class works
+define( 'WPINC', 'yeahnah' );
+
+// Load phpcs class
+require dirname( __DIR__ ) . '/includes/class-phpcs.php';
+
+$svn_tags = get_svn_tag_versions( $opts['slug'], $opts['tag' ] );
+if ( $svn_tags ) {
+	echo str_repeat( '=', 80 ) . "\n";
+	echo "Checking svn " . $opts['slug'] . " version $svn_tags[0] vs $svn_tags[1]...\n";
+	echo str_repeat( '=', 80 ) . "\n";
+	$version_strings = $svn_tags;
+
+	$path_old = export_plugin( $opts['slug'], $svn_tags[0] );
+	$path_new = export_plugin( $opts['slug'], $svn_tags[1] );
+} else {
+	$versions = get_zip_versions( $opts['slug'], $opts['tag'] );
+	if ( !$versions || count( $versions ) !== 2 ) {
+		echo "Unable to find plugin " . $opts['slug'] . " version " . $opts['tag'] . "\n";
+		#var_dump( $versions );
+		die;
+	}
+	$version_strings = array_keys( $versions );
+	$zip_urls = array_values( $versions );
+
+	echo str_repeat( '=', 80 ) . "\n";
+	echo "Checking zip " . $opts['slug'] . " version $version_strings[0] vs $version_strings[1]...\n";
+	echo str_repeat( '=', 80 ) . "\n";
+
+	$path_old = export_plugin_zip( $opts['slug'], $zip_urls[0] );
+	$path_new = export_plugin_zip( $opts['slug'], $zip_urls[1] );
+}
+
+
+$phpcs = new PHPCS();
+$phpcs->set_standard( dirname( __DIR__ ) . '/MinimalPluginStandard' );
+
+$args = array(
+	'extensions' => 'php', // Only check php files.
+	's' => true, // Show the name of the sniff triggering a violation.
+);
+
+if ( isset( $opts['errors'] ) ) {
+	$args[ 'n' ] = true;
+}
+
+
+
+$result_1 = $phpcs->run_json_report( $path_old, $args, 'array' );
+$result_2 = $phpcs->run_json_report( $path_new, $args, 'array' );
+
+#var_dump( array_diff_assoc( $result_1, $result_2 ) );
+$files = array_unique( array_merge( array_keys( $result_1[ 'files' ] ), array_keys( $result_2[ 'files' ] ) ) );
+foreach ( $files as $filename ) {
+	if ( empty( $result_1[ 'files' ][ $filename ] ) ) {
+		echo "New in $version_strings[1]: $filename\n";
+		echo "Errors " . $result_2[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_2[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+	} elseif ( empty( $result_2[ 'files' ][ $filename ] ) ) {
+		echo "Removed in $version_strings[1]: $filename\n";
+		echo "Errors " . $result_1[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_1[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+	} else {
+		if ( $result_1[ 'files' ][ $filename ][ 'errors' ] > $result_2[ 'files' ][ $filename ][ 'errors' ] || $result_1[ 'files' ][ $filename ][ 'warnings' ] > $result_2[ 'files' ][ $filename ][ 'warnings' ] ) {
+			echo "Improved in $version_strings[1]: $filename\n";
+			echo "Was Errors " . $result_1[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_1[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+			echo "Now Errors " . $result_2[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_2[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+			var_dump( $result_1[ 'files' ][ $filename ] );
+		} elseif ( $result_1[ 'files' ][ $filename ][ 'errors' ] < $result_2[ 'files' ][ $filename ][ 'errors' ] || $result_1[ 'files' ][ $filename ][ 'warnings' ] < $result_2[ 'files' ][ $filename ][ 'warnings' ] ) {
+			echo "Worse in $version_strings[1]: $filename\n";
+			echo "Was Errors " . $result_1[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_1[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+			echo "Now Errors " . $result_2[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_2[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+		} else {
+			#echo "No change in $version_strings[1]: $filename\n";
+			#echo "Errors " . $result_2[ 'files' ][ $filename ][ 'errors' ] . " and Warnings " . $result_2[ 'files' ][ $filename ][ 'warnings' ] . "\n";
+		}
+	}
+
+}
+
+#echo `diff -r -x '*.js' -x '*.txt' -x '*.mo' -x '*.po' -x '*.css' $path_old $path_new`;
+
+
